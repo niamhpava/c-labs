da763cc46b37b07d9f1a8c98ee6d676dfa104735
Niamh Perry <niamh.perry@avanade.com>
2024-11-05 14:01:00 +0000

    collections p1

---
+++ Courseware/Labs/05_Loops_and_Collections/Begin/Collections/Collections/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] muppets = { "kermit the frog", "miss piggy", "fozzie bear", "gonzo", "rowlf the dog", "scooter", "animal",
+"rizzo the rat", "pepe the prawn", "walter", "clifford"} ;
+
+
+foreach( string muppet in muppets)
+{
+    Console.WriteLine(muppet);
+}
+
+Console.WriteLine("\n");
+
+List<string> muppetList = new List<string>();
+
+foreach( string muppet in muppets)
+{
+    muppetList.Add(muppet);
+}
+
+foreach(string muppet in muppetList)
+{
+    Console.WriteLine(muppet);
+}
+
+Console.WriteLine("\n");
+
+Console.WriteLine(muppetList[0]);
+
+Console.WriteLine(muppetList[muppetList.Count - 1]);
+
+Console.WriteLine("\n");
+
+muppetList.Add("beaker");
+
+Console.WriteLine(muppetList[muppetList.Count - 1]);
