748366f5cc38928c019270a66d17f047bbab6221
Niamh Perry <niamh.perry@avanade.com>
2024-11-05 14:37:13 +0000

    collections p2

--- Courseware/Labs/05_Loops_and_Collections/Begin/Collections/Collections/Program.cs
+++ Courseware/Labs/05_Loops_and_Collections/Begin/Collections/Collections/Program.cs
@@ -32,3 +32,75 @@ Console.WriteLine("\n");
 muppetList.Add("beaker");
 
 Console.WriteLine(muppetList[muppetList.Count - 1]);
+
+Console.WriteLine("\n");
+
+foreach ( string muppet in muppetList)
+{
+    Console.Write($"{muppet}, ");
+}
+
+muppetList.Sort();
+
+Console.WriteLine("\n");
+
+foreach (string muppet in muppetList)
+{
+    Console.Write($"{muppet}, ");
+}
+
+Console.WriteLine("\n");
+
+Console.WriteLine(muppetList[0]);
+
+Console.WriteLine(muppetList[muppetList.Count - 1]);
+
+Console.WriteLine(muppetList[muppetList.Count - 2]);
+
+Console.WriteLine("\n");
+
+string slice = muppetList[5].Substring(2, 3);
+string slice2 = muppetList[6].Substring(3, 4);
+
+Console.WriteLine(slice + ", " + slice2);
+
+Console.WriteLine("\n");
+
+
+Dictionary<string, string> muppetdict = new()
+{
+    ["beaker"] = "meep",
+    ["miss piggy"] = "hi-ya!",
+    ["kermit"] = "hi-ho!",
+    ["cookie monster"] = "om nom nom"
+};
+
+var catchphrase = muppetdict["miss piggy"];
+
+Console.WriteLine(catchphrase);
+
+Console.WriteLine("\n");
+
+
+foreach (KeyValuePair<string, string> kvp in muppetdict)
+{
+    Console.WriteLine(kvp.ToString());
+}
+
+Console.WriteLine("\n");
+
+
+foreach (string muppet in muppetdict.Keys)
+{
+    Console.Write($"{muppet}, "); 
+}
+
+Console.WriteLine("\n");
+
+
+foreach (string catchphrase2 in muppetdict.Values)
+{
+    Console.Write($"{catchphrase2}, ");
+}
+
+Console.WriteLine("\n");
